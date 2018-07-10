26ffaff1410852ffe83689eddc56560fc91d6fbe
remi-san <remi.san@gmail.com>
2018-07-10 18:53:04 +0200

    feat(dbal): add a tx manager for Docrine dbal connection not respecting the connection contract

---
+++ src/Doctrine/DoctrineDbalConnectionTransactionManager.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace RemiSan\TransactionManager\Doctrine;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\ConnectionException;
+use RemiSan\TransactionManager\Exception\BeginException;
+use RemiSan\TransactionManager\Exception\CommitException;
+use RemiSan\TransactionManager\Exception\RollbackException;
+use RemiSan\TransactionManager\Transactional;
+
+final class DoctrineDbalConnectionTransactionManager implements Transactional
+{
+    /**
+     * @var Connection
+     */
+    private $dbalConnection;
+
+    /**
+     * Constructor.
+     *
+     * @param Connection $dbalConnection
+     */
+    public function __construct(Connection $dbalConnection)
+    {
+        $this->dbalConnection = $dbalConnection;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function beginTransaction()
+    {
+        try {
+            $this->dbalConnection->beginTransaction();
+        } catch (\Exception $e) {
+            throw new BeginException('Cannot begin Doctrine DBAL transaction');
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function commit()
+    {
+        try {
+            $this->dbalConnection->commit();
+        } catch (ConnectionException $e) {
+            throw new CommitException('Cannot commit Doctrine DBAL transaction');
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rollback()
+    {
+        try {
+            $this->dbalConnection->commit();
+        } catch (ConnectionException $e) {
+            throw new RollbackException('Cannot rollback Doctrine DBAL transaction');
+        }
+    }
+}
